03009ac7886abcce18b4b3475eaa5b706224ba4c
Santhosh Mamidisetti <92091342+SANTHOSH-MAMIDISETTI@users.noreply.github.com>
2023-09-29 11:27:37 +0530

    Time: 6 ms (37.65%), Space: 8.6 MB (20.29%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -1,37 +1,3 @@
-// class Solution {
-// public:
-//     int calPoints(vector<string>& operations) {
-//         stack<int> stack;
-//         int sum=0;
-//         for(int i=0;i<operations.size();i++)
-//         {
-//             if(operations[i]=="+")
-//             {
-//                 int one = stack.top();
-//                 stack.pop();
-//                 int two = stack.top();
-//                 stack.pop();
-//                 stack.push(one+two);
-//                 sum +=one+two;
-//             }
-//             else if(operations[i]=="D")
-//             {
-//                 sum += 2*stack.top();
-//                 stack.push(2*stack.top());
-//             }
-//             else if(operations[i]=="C")
-//             {
-//                 sum -= stack.top();
-//                 stack.pop();
-//             }
-//             else{
-//                 sum += stoi(operations[i]);
-//                 stack.push(stoi(operations[i]));
-//             }
-//         }
-//         return sum;
-//     }
-// };
 class Solution {
 public:
     int calPoints(vector<string>& ops) {
