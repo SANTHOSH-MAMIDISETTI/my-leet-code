259301632e36db3e04e1185cb9a922d42331466a
Santhosh Mamidisetti <92091342+SANTHOSH-MAMIDISETTI@users.noreply.github.com>
2024-03-27 05:26:36 +0530

    Time: 22 ms (46.58%), Space: 27.4 MB (57.08%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,14 +1,24 @@
+// class Solution {
+// public:
+// //     O(n) time & O(n) space
+//     void rotate(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         k = k%n;
+//         vector <int> sol(n);
+//         for(int i=0;i<n;i++)
+//         {
+//             sol[(i+k)%n]=nums[i];
+//         }
+//         nums=sol;
+//     }
+// };
 class Solution {
-public:
-//     O(n) time & O(n) space
+    public:
+    // O(n) time and O(1) space
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k%n;
-        vector <int> sol(n);
-        for(int i=0;i<n;i++)
-        {
-            sol[(i+k)%n]=nums[i];
-        }
-        nums=sol;
+            k = k%nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
