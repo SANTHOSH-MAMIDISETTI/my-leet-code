bfddb9b26bab621c9127535c375c63dcc6df8b82
Santhosh Mamidisetti <92091342+SANTHOSH-MAMIDISETTI@users.noreply.github.com>
2024-10-25 18:50:43 +0530

    Time: 19 ms (88.7%), Space: 77.6 MB (19.4%) - LeetHub

---
+++ 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums) 
+    {
+        map<int, vector<int>> DigitMap;
+        
+        for (int num : nums) {
+            int Largest = MaxDigit(num);
+            DigitMap[Largest].push_back(num);
+        }
+
+        int maxSum = -1;
+
+        for (const auto& entry : DigitMap) {
+            const auto& keys = entry.second;
+            if (keys.size() > 1) {
+                vector<int> temp(keys); 
+                sort(temp.begin(), temp.end(), greater<int>());
+                int sum = temp[0] + temp[1];
+                maxSum = max(maxSum, sum);
+            }
+        }
+    return maxSum;
+    }
+
+private:
+    int MaxDigit(int number)
+    {
+        int Largest_num = 0;
+        while (number > 0)
+        {
+            int digit = number % 10;
+            Largest_num = max(Largest_num, digit);
+            number /= 10;
+        }
+    return Largest_num;
+    }
+};
